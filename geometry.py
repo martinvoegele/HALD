71592bb3f6536c5a51cdc4acd5a449dde96d1f6a
ruyincao <r.cao@grs-sim.de>
2015-10-15 12:13:10 +0200

    add the tilt angle function

--- geometry.py
+++ geometry.py
@@ -1,4 +1,6 @@
 import numpy as np
+import numpy.linalg as la
+
 
 def dist_point_line(p,a,n):
     """Calculates the distance of a point to a line
@@ -14,3 +16,22 @@ def dist_point_line(p,a,n):
     distance = (a-p)-np.dot((a-p),n)*n
     
     return np.linalg.norm(distance)
+
+def Tilt(calpha):
+    """
+    Purpose:
+    calculate the angle between the principal axis of one alpha
+    helix and the z-axis
+    The principal axis of the helix is calculated using the MDAnalysis
+    function
+    
+    Arguments:
+    calpha: alpha atoms object within MDAnalysis
+    """
+    P1,P2,P3=calpha.principal_axes()
+    zaxis=np.array([0,0,1])
+    
+    """angle"""
+    cosang = np.dot(P1, zaxis)d
+    sinang = la.norm(np.cross(P1, zaxis))
+    return np.arctan2(sinang, cosang)
