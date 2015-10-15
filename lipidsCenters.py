e8a9e538594b761a58411ed355a9c23de4b0c733
Kasia Ziolkowska <katarzyna.ziolk@gmail.com>
2015-10-15 13:41:00 +0200

    method for computing lipids centers of mass

---
+++ lipidsCenters.py
@@ -0,0 +1,46 @@
+#selecting membrane, lipid tail and heads
+def membrane_mass_centers(universe, lipidresiduename, separationatomname):
+    #select the mambrane:
+    membrane = universe.residues[universe.residues.resnames == lipidresiduename]
+    
+    #separete heads
+    heads = []
+    for l in membrane:
+        lat = l.atoms
+        itemindex = np.where(lat.names==separationatomname)
+        head = lat[:itemindex[0][0]]
+        heads.append(head)
+        
+    #centers of mass of heads:
+    headsCenters = []
+    for h in heads:
+        headsCenters.append(h.center_of_mass())
+    
+    #separate tails:
+    tails = []
+    for l in membrane:
+        lat = l.atoms
+        itemindex = np.where(lat.names==separationatomname)
+        tail = lat[itemindex[0][0]:]
+        tails.append(tail)
+        
+    #centers of mass of heads:
+    tailsCenters = []
+    for t in tails:
+        tailsCenters.append(t.center_of_mass())
+    
+    #centers of mass of lipids:
+    membraneCenters = []
+    for l in membrane:
+        lat = l.atoms
+        membraneCenters.append(lat.center_of_mass())
+
+    #seperate tail1:
+
+    #seperate tail2:
+    
+    #center of mass of tails1:
+    
+    #center of mass of tails2:
+        
+    return membraneCenters, headsCenters, tailsCenters
